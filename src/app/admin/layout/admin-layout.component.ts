908e0e735e88f510e4a599ec0247535f1afa2406
Watanuki <ore395@hotmail.com>
2023-08-02 18:55:14 -0600

    Adding new wording

--- src/app/admin/layout/admin-layout.component.ts
+++ src/app/admin/layout/admin-layout.component.ts
@@ -1,10 +1,13 @@
 import {Component} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({ templateUrl: 'admin-layout.component.html', styleUrls: ['./admin-layout.component.css'] })
 export class AdminLayoutComponent {
 
   constructor(
+    private router: Router,
   ) {
+    this.router.navigate(['/admin/users']);
   }
 
 }
