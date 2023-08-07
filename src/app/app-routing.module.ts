2edd71b593c0cf2149499a40179a7f16be7211a9
Yehoshua M.V <yosh.mataval@gmail.com>
2023-08-06 21:17:32 -0600

    arreglando rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home';
 import {AuthGuard} from './_helpers';
-import {InitialPageComponent} from "./initial-page";
+import {InitialPageComponent} from './initial-page';
 
 const securityModule = () => import('./security/security.module').then(x => x.SecurityModule);
 const adminModule = () => import('./admin/admin.module').then(x => x.AdminModule);
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  {path: 'admin', loadChildren: adminModule},
+  {path: 'admin', loadChildren: adminModule, canActivate: [AuthGuard]},
   {path: 'security', loadChildren: securityModule},
   {path: '', component: InitialPageComponent},
   // otherwise redirect to home
